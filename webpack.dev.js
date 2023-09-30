10f998de0e74f7c23798a361dc3b744df3b999ae
Hans Beemsterboer <hpbeem@gmail.com>
2023-09-30 17:28:54 +0200

    Fixed localhost connection

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const { merge } = require('webpack-merge');
+const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const fs = require('fs');
 
@@ -6,7 +6,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        host: 'localhost',
+        host: '0.0.0.0', // Allows external connections to localhost
         port: 8081,
         server: {
             type: 'https',
